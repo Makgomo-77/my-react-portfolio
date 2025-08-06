04713966254e2d0eb15191850566b1db28912dbf
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-08-06 09:26:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 
+import React from "react";
+import NavigationBar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
