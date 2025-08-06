fd961b312172f32d0c39e4513acb0c15cf19f7e4
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-08-06 09:23:49 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,1 +1,10 @@
 
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p className="mb-0">&copy; {new Date().getFullYear()} Makgomo Shayi | All Rights Reserved</p>
+    </footer>
+  );
+}
