d2ecaf35894a33af625b36b7e1b74df698262eef
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-08-06 09:27:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({ duration: 1000, once: true });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
